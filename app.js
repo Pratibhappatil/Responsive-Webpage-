dd8777726fc4a22906430a6e7b9589c52603a175
Pratibha Patil <62788513+Pratibhappatil@users.noreply.github.com>
2020-09-19 01:36:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const hamberger = document.querySelector('.header .nav-bar .nav-list .hamberger')
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul')
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul a')
+const header = document.querySelector('.header .container')
+hamberger.addEventListener('click', () => {
+ hamberger.classList.toggle('active');
+ mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+ var scroll_position = window.scrollY;
+ if (scroll_position > 250) {
+  header.style.backgroundColor = "#29323c";
+
+ } else {
+  header.style.backgroundColor = "transparent"
+ }
+});
+menu_item.forEach((item) => {
+ item.addEventListener('click', () => {
+  hamberger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+ });
+
+});
